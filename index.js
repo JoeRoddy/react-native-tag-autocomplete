5bb71c0db9f8dae928ce7c640ff2c8e1f1d85920
Joe <mrjoeroddy@gmail.com>
2017-07-30 12:37:20 -0400

    fix dependencies and bump vers

--- index.js
+++ index.js
@@ -122,6 +122,7 @@ const styles = StyleSheet.create({
         borderRadius: 0,
         paddingLeft: 5,
         height: 40,
+        width: 300,
         justifyContent: 'center',
         borderColor: 'transparent',
         alignItems: 'stretch',
